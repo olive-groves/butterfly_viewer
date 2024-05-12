d0f156c97f50833afcc1d7a4a66bc4eec2e345eb
Lars Maxfield <maxfield.lars@gmail.com>
2024-05-12 13:39:54 +0200

    Move relative sync functions to seperate function for re-use in individual SplitViews

--- butterfly_viewer/aux_functions.py
+++ butterfly_viewer/aux_functions.py
@@ -14,6 +14,74 @@ Credits:
 from PyQt5 import QtCore
 
 
+def determineSyncSenderDimension(width: int,
+                                 height: int,
+                                 sync_by: str="box"):
+    """Get the dimension of the sender image with which to synchronize zoom.
+    
+    Args:
+        width (int): Width of sender in pixels.
+        height (int): Height of sender in pixels.
+        sync_by (str): Method by which to sync zoom ("box", "width", "height", "pixel").
+
+    Returns:
+        dimension (int, None): Dimension with which to synchronize zoom (None if sync by pixel).
+    """
+
+    if sync_by == "width":
+        dimension = width
+    elif sync_by == "height":
+        dimension = height
+    elif sync_by == "pixel":
+        dimension = None
+    else:  # Equivalent to sync_by == "box"
+        # Tall image means the height dictates the size of the zoom box.
+        # Wide image means the width dictates the size of the zoom box.
+        if height >= width:
+            dimension = height
+        else:
+            dimension = width
+
+    return dimension
+
+
+
+def determineSyncAdjustmentFactor(sync_by: str,
+                                  sender_dimension: int,
+                                  receiver_width: int,
+                                  receiver_height: int):
+    """Get the factor with which to multiply the zoom of the sender before giving it to the receiver to synchronize them.
+
+    Returns 1 if receiver width or height are zero.
+    
+    Args:
+        sync_by (str): Method by which to sync zoom ("box", "width", "height", "pixel").
+        sender_dimension (int): Dimension of sender in pixels, as determined by determineSyncSenderDimension().
+        receiver_width (int): Width of receiver in pixels.
+        receiver_height (int): Height of sender in pixels.
+
+    Returns:
+        adjustment_factor (float): Factor with which to multiply the sender zoom to sync the receiver.
+    """
+
+    if (receiver_width == 0) or (receiver_height == 0):
+        return 1
+
+    if sync_by == "width":
+        adjustment_factor = sender_dimension/receiver_width
+    elif sync_by == "height":
+        adjustment_factor = sender_dimension/receiver_height
+    elif sync_by == "pixel":
+        adjustment_factor = 1.0
+    else: # "box"
+        if receiver_width >= receiver_height:
+            adjustment_factor = sender_dimension/receiver_width
+        else:
+            adjustment_factor = sender_dimension/receiver_height
+    
+    return adjustment_factor
+
+
 
 def strippedName(fullFilename):
     """Get filename from a filepath.
